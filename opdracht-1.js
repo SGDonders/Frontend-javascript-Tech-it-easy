f68eb0de19646985182ae0aca9f03fcfa6a7956d
SGDonders <donders_sg@live.com>
2022-11-06 01:56:14 +0100

    first commit

---
+++ opdracht-1.js
@@ -0,0 +1,29 @@
+// OPDRACHT 1 - ARRAY METHODEN
+
+// Opdracht 1a:
+// Gebruik een array-methode om een array te maken met alle tv-type namen.
+// Log de uitkomst in de console.
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Opdracht 1b:
+// Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht zijn.
+// Log de uitkomst in de console.
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Opdracht 1c:
+// Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight beschikken.
+// Log de uitkomst in de console.
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Opdracht 1d:
+// Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert.
+// Log de uitkomst in de console.
---
+++ opdracht-2.js
@@ -0,0 +1,34 @@
+// OPDRACHT 2 - ELEMENTEN IN DE DOM PLAATSEN
+
+// Opdracht 2a:
+// Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent.
+// Log de uitkomst in de console.
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Opdracht 2b:
+// Zorg ervoor dat dit aantal in het groen wordt weergegeven op de pagina.
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Opdracht 2c:
+// Hoeveel tv's heeft Tech It Easy ingekocht? Schrijf een script dat dit berekent.
+// Log de uitkomst in de console.
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Opdracht 2d:
+// Zorg ervoor dat dit aantal in het blauw wordt weergegeven op de pagina.
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Opdracht 2e:
+// Geef in het rood weer hoeveel tv's er nog verkocht moeten worden.
